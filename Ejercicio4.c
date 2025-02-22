0ed1c097079c361bae084f9df8d620be146dc83d
Gustavo <cru22779@uvg.edu.gt>
2025-02-21 21:53:06 -0600

    Modified: Ejercicio 4

--- Ejercicio4.c
+++ Ejercicio4.c
@@ -3,13 +3,19 @@
 
 int main() {
     pid_t pid = fork();
+
     if (pid == 0) {
-        // Proceso hijo
+        for (int i = 1; i <= 4000000; i++) {
+            if (i % 1000000 == 0) {
+				sleep(1000);
+                printf("Hijo: %d\n", i);
+            }
+        }
         printf("Let's all love Lain\n");
     } else {
-        // Proceso padre
         while (1);
     }
+
     return 0;
 }
 
